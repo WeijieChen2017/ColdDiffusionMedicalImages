afd4e06aab420b494271d4f7e1228a67c5a77cc8
WeijieChen2017 <weijiechen1994@gmail.com>
2023-10-31 00:08:13 -0500

    Good morning!

---
+++ MR2CT_2d/demixing_diffusion_pytorch/simple_trainer_PVC.py
@@ -0,0 +1,220 @@
+# from comet_ml import Expeiment
+import gc
+import copy
+import torch
+from torch import nn
+from functools import partial
+
+from pathlib import Path
+from torch.optim import Adam
+from torchvision import utils
+
+try:
+    from apex import amp
+    APEX_AVAILABLE = True
+except:
+    APEX_AVAILABLE = False
+
+import torch
+# import l1 loss and l2 loss
+from torch.nn import L1Loss, MSELoss
+
+from .network import EMA
+from .utils import loss_backwards
+
+# trainer class
+
+class simple_trainer(object):
+    def __init__(
+        self,
+        model,
+        data_loader,
+        *,
+        ema_decay = 0.995,
+        image_size = 128,
+        time_steps = 1000,
+        loss_type = 'l1',
+        train_batch_size = 32,
+        train_lr = 2e-5,
+        train_num_steps = 100000,
+        gradient_accumulate_every = 2,
+        fp16 = False,
+        step_start_ema = 2000,
+        update_ema_every = 10,
+        save_and_sample_every = 1000,
+        results_folder = './results',
+        load_path = None,
+    ):
+        super().__init__()
+        self.model = model
+        self.ema = EMA(ema_decay)
+        self.ema_model = copy.deepcopy(self.model)
+        self.update_ema_every = update_ema_every
+
+        self.step_start_ema = step_start_ema
+        self.save_and_sample_every = save_and_sample_every
+
+        self.batch_size = train_batch_size
+        self.image_size = image_size
+        self.gradient_accumulate_every = gradient_accumulate_every
+        self.train_num_steps = train_num_steps
+
+        self.opt = Adam(model.parameters(), lr=train_lr)
+        self.step = 0
+
+        self.results_folder = Path(results_folder)
+        self.results_folder.mkdir(exist_ok = True)
+
+        self.fp16 = fp16
+
+        self.reset_parameters()
+
+        if load_path != None:
+            self.load(load_path)
+
+        self.time_steps = time_steps
+        self.loss_type = loss_type
+        self.loss_fn = L1Loss() if loss_type == 'l1' else MSELoss()
+        self.dataloader = data_loader
+        self.max_time = torch.tensor(self.time_steps, dtype=torch.float)
+        self.max_time = self.max_time.expand(self.batch_size).to(device='cuda')
+
+    def reset_parameters(self):
+        self.ema_model.load_state_dict(self.model.state_dict())
+
+    def step_ema(self):
+        if self.step < self.step_start_ema:
+            self.reset_parameters()
+            return
+        self.ema.update_model_average(self.ema_model, self.model)
+
+    def save(self, itrs=None):
+        data = {
+            'step': self.step,
+            'model': self.model.state_dict(),
+            'ema': self.ema_model.state_dict()
+        }
+        if itrs is None:
+            torch.save(data, str(self.results_folder / f'model.pt'))
+        else:
+            torch.save(data, str(self.results_folder / f'model_{itrs}.pt'))
+
+    def load(self, load_path):
+        print("Loading : ", load_path)
+        data = torch.load(load_path)
+
+        self.step = data['step']
+        self.model.load_state_dict(data['model'])
+        self.ema_model.load_state_dict(data['ema'])
+
+    def generate_xt1_xt2(self, data_1, data_2, device):
+        # generate t_1 and t_2. t_2 is from 2 to self.time_steps, and t1 is from 1 to t_2 - 1
+        t_2_int = torch.randint(3, self.time_steps, (1,))
+        t_1_int = torch.randint(1, t_2_int-1, (1,))
+        assert t_1_int < t_2_int
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # make both t_1 and t_2 of the shape of batch_size
+        t_1 = t_1_int.expand(data_1.shape[0])
+        t_2 = t_2_int.expand(data_1.shape[0])
+
+        alpha_1 = t_1_int.float() / self.time_steps
+        alpha_2 = t_2_int.float() / self.time_steps
+
+        # Explicitly broadcasting alpha_1 and alpha_2
+        alpha_1 = alpha_1.view(-1, 1, 1, 1)
+        alpha_2 = alpha_2.view(-1, 1, 1, 1)
+
+        data_t1 = (1 - alpha_1) * data_1 + (alpha_1) * data_2
+        data_t2 = (1 - alpha_2) * data_1 + (alpha_2) * data_2
+
+        # move to device
+        data_t1 = data_t1.to(device)
+        data_t2 = data_t2.to(device)
+        t_1 = t_1.to(device)
+        t_2 = t_2.to(device)
+
+        return data_t1, data_t2, t_1, t_2, t_1_int, t_2_int
+
+    def train(self):
+        # experiment = Experiment(api_key="57ArytWuo2X4cdDmgU1jxin77",
+        #                         project_name="Cold_Diffusion_Cycle")
+
+        backwards = partial(loss_backwards, self.fp16)
+
+        acc_loss = 0
+        while self.step < self.train_num_steps:
+            u_loss = 0
+            for i in range(self.gradient_accumulate_every):
+                data_1, data_2 = next(self.dataloader)
+                
+                data_t1, data_t2, t_1, t_2, _, _ = self.generate_xt1_xt2(data_1, data_2, device='cuda')
+
+                data_t2_hat = self.model(data_t1, t_2-t_1)
+                loss = self.loss_fn(data_t2_hat, data_t2)
+
+                if self.step % 100 == 0:
+                    print(f'{self.step}: {loss.item()}')
+                u_loss += loss.item()
+                backwards(loss / self.gradient_accumulate_every, self.opt)
+
+            acc_loss = acc_loss + (u_loss/self.gradient_accumulate_every)
+
+            self.opt.step()
+            self.opt.zero_grad()
+
+            if self.step % self.update_ema_every == 0:
+                self.step_ema()
+
+            if self.step != 0 and self.step % self.save_and_sample_every == 0:
+                # experiment.log_current_epoch(self.step)
+                milestone = self.step // self.save_and_sample_every
+                data_1, data_2 = next(self.dataloader)
+                data_t1, data_t2, t_1, t_2, t_1_int, t_2_int = self.generate_xt1_xt2(data_1, data_2, device='cuda')
+                # create max_time as a tensor of shape batch_size
+                # given that the max_time is self.time_steps as a int
+                data_t2_hat = self.model(data_t1, t_2-t_1)
+                # data_1 = data_1.to(device='cuda')
+                # data_syn_t2 = self.model(data_1, self.max_time)
+
+                imgs_to_plot = [
+                    # imgs, title
+                    [data_1, 'MR'],
+                    [data_2, 'CT'],
+                    # [data_syn_t2, 'synCT'],
+                    # include the time step t_1_int in the title
+                    [data_t1, f'data_t1_{int(t_1_int)}'],
+                    [data_t2, f'data_t2_{int(t_2_int)}'],
+                    [data_t2_hat, 'data_t2_hat'],
+                ]
+
+                # iteratively plot
+                for imgs, title in imgs_to_plot:
+                    imgs = imgs.detach().cpu()
+                    utils.save_image(imgs, str(self.results_folder / f'{title}-{milestone}.png'), nrow=4)
+                    del imgs
+
+                gc.collect()
+
+                acc_loss = acc_loss/(self.save_and_sample_every+1)
+                # experiment.log_metric("Training Loss", acc_loss, step=self.step)
+                print(f'Mean of last {self.step}: {acc_loss}')
+                acc_loss=0
+
+                self.save()
+                if self.step % (self.save_and_sample_every * 100) == 0:
+                    self.save(self.step)
+
+            self.step += 1
+
+        print('training completed')
---
+++ MR2CT_2d/preprocess_PVC_split.py
@@ -0,0 +1,121 @@
+# this file is used to preprocess the dataset. We need to do the following things:
+# 1. load two folders, one for the input data, one for the output data
+# 2. construct the file list for both folders, requiring the file name of the input data and the output data are the same
+# 3. construct the train/val/test split according to the given ratio, and save the split as a json file
+# 4. create the folders for train/val/test split, and move files into them
+# 5. for each folder, run the function prepare_folder to save the files as *.npy 
+
+import os
+import json
+import numpy as np
+import argparse
+import glob
+
+# from util import prepare_folder
+
+# load the two folders, and the split ratio and random seed
+parser = argparse.ArgumentParser()
+parser.add_argument('--x_path', default='./data/MR2CT/MR', type=str)
+parser.add_argument('--train_ratio', default=0.7, type=float)
+parser.add_argument('--val_ratio', default=0.2, type=float)
+parser.add_argument('--test_ratio', default=0.1, type=float)
+parser.add_argument('--seed', default=1024, type=int)
+args = parser.parse_args()
+print(args)
+
+# load the two folders, and the split ratio
+x_path = args.x_path
+train_ratio = args.train_ratio
+val_ratio = args.val_ratio
+test_ratio = args.test_ratio
+
+# construct the file list
+x_file_list = glob.glob(x_path + "/*.nii.gz")
+print("MR files num: ", len(x_file_list))
+
+# sort the file list
+x_file_list.sort()
+    
+# construct the train/val/test split
+num_files = len(x_file_list)
+num_train = int(num_files * train_ratio)
+num_val = int(num_files * val_ratio)
+num_test = num_files - num_train - num_val
+print("num_train: ", num_train, "num_val: ", num_val, "num_test: ", num_test)
+
+# create the folders for train/val/test split
+train_folder = "./data/PVC/train/"
+val_folder = "./data/PVC/val/"
+test_folder = "./data/PVC/test/"
+
+train_folder_list = []
+val_folder_list = []
+test_folder_list = []
+
+# for each split, create MR and CT folders
+for folder in [train_folder, val_folder, test_folder]:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+        print("create folder: ", folder)
+
+# set the random seed, create a file list and shuffle it
+np.random.seed(args.seed)
+file_list = []
+for idx in range(num_files):
+    file_list.append(x_file_list[idx])
+file_list = np.array(file_list)
+np.random.shuffle(file_list)
+
+# move files into the folders
+for idx in range(num_train):
+    x_filename = file_list[idx, 0]
+    savename_x = train_folder + "MR/" + os.path.basename(x_filename)
+    train_folder_list.append({
+        "MR": savename_x,
+    })
+    os.system("mv {} {}".format(x_filename, savename_x))
+    print("mv {} {}".format(x_filename, savename_x))
+
+for idx in range(num_train, num_train + num_val):
+    x_filename = file_list[idx, 0]
+    savename_x = val_folder + "MR/" + os.path.basename(x_filename)
+    val_folder_list.append({
+        "MR": savename_x,
+    })
+    os.system("mv {} {}".format(x_filename, savename_x))
+    print("mv {} {}".format(x_filename, savename_x))
+
+for idx in range(num_train + num_val, num_files):
+    x_filename = file_list[idx, 0]
+    savename_x = test_folder + "MR/" + os.path.basename(x_filename)
+    test_folder_list.append({
+        "MR": savename_x,
+    })
+    os.system("mv {} {}".format(x_filename, savename_x))
+    print("mv {} {}".format(x_filename, savename_x))
+
+# save the split as a json file, and save the json file
+json_file = "./data/MR2CT/split.json"
+split_dict = {
+    "train": train_folder_list,
+    "val": val_folder_list,
+    "test": test_folder_list
+}
+with open(json_file, "w") as f:
+    json.dump(split_dict, f, indent=4)
+f.close()
+print("save json file: ", json_file)
+
+# # for each folder, run the function prepare_folder to save the files as *.npy
+# for folder in [train_folder, val_folder, test_folder]:
+#     prepare_folder(folder + "MR/", modality="MR")
+#     prepare_folder(folder + "CT/", modality="CT")
+
+
+
+
+
+
+
+
+
