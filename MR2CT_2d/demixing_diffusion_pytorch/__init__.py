0d8b7a5f7037a7cac231cce0ac5f1438395dbd0f
WeijieChen2017 <weijiechen1994@gmail.com>
2023-10-31 21:40:09 -0500

    Good morning!

--- MR2CT_2d/demixing_diffusion_pytorch/__init__.py
+++ MR2CT_2d/demixing_diffusion_pytorch/__init__.py
@@ -4,6 +4,6 @@ from .network import LayerNorm, PreNorm, LinearAttention, ConvNextBlock, Unet
 from .diffusion import GaussianDiffusion
 from .dataset import DatasetPaired_Aug, DatasetPaired
 # from .trainer import Trainer
-from .period_trainer_MR2CT import period_trainer
+from .period_trainer_MR2CT import period_trainer_MR2CT
 from .period_trainer_PVC import period_trainer_PVC
 from .instance_trainer_PVC import instance_trainer_PVC
