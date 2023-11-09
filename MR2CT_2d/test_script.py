4e7a8e7d3622393dacbc481ead008a53157be43c
WeijieChen2017 <weijiechen1994@gmail.com>
2023-11-09 12:10:06 -0600

    Good morning!

--- MR2CT_2d/test_script.py
+++ MR2CT_2d/test_script.py
@@ -1,18 +1,18 @@
 import os
 
 cmd_list = [
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 1",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 2",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 3",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 4",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 5",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 9",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 1",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 1",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 2",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 3",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 4",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 5",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 9",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 1",
     "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 2",
     "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 3",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 4",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 5",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 9",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 4",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 5",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 9",
 ]
 
 for cmd in cmd_list:
