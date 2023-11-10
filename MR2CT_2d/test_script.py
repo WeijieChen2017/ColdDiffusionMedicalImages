6f0967e99643aadd3c223342d1e1aa6a6d044688
WeijieChen2017 <weijiechen1994@gmail.com>
2023-11-10 01:10:50 -0600

    Good morning!

--- MR2CT_2d/test_script.py
+++ MR2CT_2d/test_script.py
@@ -7,12 +7,17 @@ cmd_list = [
     # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 4",
     # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 5",
     # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_200000.pt --n_jumps 9",
-    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 1",
+    # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 1",
     # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 2",
     # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 3",
     # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 4",
     # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 5",
     # "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_200000.pt --n_jumps 9",
+    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_300000.pt --n_jumps 1",
+    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_300000.pt --n_jumps 2",
+    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeB/model_300000.pt --n_jumps 4",
+    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_300000.pt --n_jumps 1",
+    "python MR2CT_simpleUNet_test.py --load_path ./proj/MR2CT_typeA/model_300000.pt --n_jumps 2",
 ]
 
 for cmd in cmd_list:
